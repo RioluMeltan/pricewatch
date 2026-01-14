93b51acec2ad7534591251f088835116aac65437
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2026-01-14 03:22:16 +0000

    More class variables

--- MainPage.py
+++ MainPage.py
@@ -1,8 +1,13 @@
+# Import statements
 import streamlit as st
+import PIL
 
 # Static class to represent the homepage
 class MainPage: 
 
     # Variable definitions
-    mainLabel = st.title('PriceWatch: A Price Comparison Tool for Fair Resale Valuation of Luxury Watches')
-    logo = st.image()
+    mainLabel = 'PriceWatch: A Price Comparison Tool for Fair Resale Valuation of Luxury Watches'
+    logo = PIL.Image.open()
+    addWatch = 'Add Watch'
+    enterWatch = 'Enter Watch Below'
+    watchTextInput = st.sidebar.text_input('Enter watch here')
