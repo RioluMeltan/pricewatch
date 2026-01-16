6a24ee275336a7aeb2715382525174638d5e0f12
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2026-01-16 01:12:15 +0000

    Main page variables

--- MainPage.py
+++ MainPage.py
@@ -6,8 +6,21 @@ import PIL
 class MainPage: 
 
     # Variable definitions
-    mainLabel = 'PriceWatch: A Price Comparison Tool for Fair Resale Valuation of Luxury Watches'
-    logo = PIL.Image.open()
-    addWatch = 'Add Watch'
-    enterWatch = 'Enter Watch Below'
-    watchTextInput = st.sidebar.text_input('Enter watch here')
+    mainLabel = st.title('PriceWatch: A Price Comparison Tool for Fair Resale Valuation of Luxury Watches')
+    logo = st.sidebar.image(PIL.Image.open())
+    addWatch = st.sidebar.text('Add Watch')
+    enterWatch = st.sidebar.text('Enter Watch Below')
+    watchTextBox = st.sidebar.text_input('Enter watch here')
+    searchWatchButton = st.sidebar.button('Search for Watch')
+    importListButton = st.sidebar.button('Import List from Clipboard')
+
+    # Column formatting for buttons
+    col_1, col_2, col_3 = st.columns([1, 1, 1])
+    with col_1: 
+        currencyConvButton = st.button('Currency Converter')
+
+    with col_2: 
+        marginCalcButton = st.button('Margin Calculator')
+
+    with col_3: 
+        exportListButton = st.button('Export Watchlist')
