ef115a150a8e04afe170d59de4b36d57cfc4d161
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2026-01-24 14:24:30 +0000

    Made a syntax mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,63 +9,63 @@ from Watch import Watch
 from Watch import SearchedWatch
 
 # Static method to search for watches and create a modal based on the given search terms, data is also cached if ever rereloaded
-    @staticmethod
-    @st.cache_data
-    @st.dialog(title = 'Results for Your Search', width = 'large', dismissible = True)
-    def searchWatchesModal(term): 
+@staticmethod
+@st.cache_data
+@st.dialog(title = 'Results for Your Search', width = 'large', dismissible = True)
+def searchWatchesModal(term): 
 
-        # Variable initialization
-        resultList = []
-        query = []
+    # Variable initialization
+    resultList = []
+    query = []
 
-        # Exception handling
-        try: 
+    # Exception handling
+    try: 
 
-            # Parameters and headers for eBay
-            client_id = st.secrets['CLIENT_ID']
-            client_secret = st.secrets['CLIENT_SECRET']
-            auth = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
-            headers = {'Authorization': f'Basic {auth}', 'Content-Type': 'application/x-www-form-urlencoded'}
-            data = {'grant_type': 'client_credentials', 'scope': 'https://api.ebay.com/oauth/api_scope'}
-            token = requests.post('https://api.ebay.com/identity/v1/oauth2/token', headers = headers, data = data).json()['access_token']
+        # Parameters and headers for eBay
+        client_id = st.secrets['CLIENT_ID']
+        client_secret = st.secrets['CLIENT_SECRET']
+        auth = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
+        headers = {'Authorization': f'Basic {auth}', 'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {'grant_type': 'client_credentials', 'scope': 'https://api.ebay.com/oauth/api_scope'}
+        token = requests.post('https://api.ebay.com/identity/v1/oauth2/token', headers = headers, data = data).json()['access_token']
 
-            # Querying for watches
-            headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
-            query = requests.get('https://api.ebay.com/buy/browse/v1/item_summary/search', headers = headers, params = {'q': term, 'limit': 20}).json().get('itemSummaries', [])
+        # Querying for watches
+        headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+        query = requests.get('https://api.ebay.com/buy/browse/v1/item_summary/search', headers = headers, params = {'q': term, 'limit': 20}).json().get('itemSummaries', [])
 
-        except: 
-
-            # Exception handling
-            print('Request unsuccessful. Try again later.')
+    except: 
 
         # Exception handling
-        try: 
-        
-            # Iterating through list
-            for i in query:  
+        print('Request unsuccessful. Try again later.')
 
-                # Obtaining an image from a public URL
-                try: 
-                    response = requests.get(i['image']['imageUrl']).content
-                except: 
-                    print('Image access failed.')
+    # Exception handling
+    try: 
+    
+        # Iterating through list
+        for i in query:  
 
-                # Initiating a watch object based on info returned
-                resultList.append(SearchedWatch(PIL.Image.open(io.BytesIO(response)), i['title'], float(i['price']['value']), i['price']['currency']))
+            # Obtaining an image from a public URL
+            try: 
+                response = requests.get(i['image']['imageUrl']).content
+            except: 
+                print('Image access failed.')
 
-        except: 
+            # Initiating a watch object based on info returned
+            resultList.append(SearchedWatch(PIL.Image.open(io.BytesIO(response)), i['title'], float(i['price']['value']), i['price']['currency']))
 
-            # Exception handling
-            print('Iteration failed.')
+    except: 
 
-        # Displaying results using columns and enumerated list to cycle
-        cols = st.columns(3)
-        for index, watch in enumerate(resultList): 
-            col = cols[index % 3]
-            with col:
-                st.image(watch.getIcon(), use_column_width = True)
-                st.markdown(watch.getName())
-                st.markdown(f'${watch.getPrice()} {watch.getCurrency()}')
+        # Exception handling
+        print('Iteration failed.')
+
+    # Displaying results using columns and enumerated list to cycle
+    cols = st.columns(3)
+    for index, watch in enumerate(resultList): 
+        col = cols[index % 3]
+        with col:
+            st.image(watch.getIcon(), use_column_width = True)
+            st.markdown(watch.getName())
+            st.markdown(f'${watch.getPrice()} {watch.getCurrency()}')
 
 if st.button('testing'): 
     searchWatchesModal('rolex daytona')
