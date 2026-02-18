730c1683e52b111b6f15e5b089cfd354a1e8bd03
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2026-02-18 05:37:10 +0000

    Small changes again

--- MainPage.py
+++ MainPage.py
@@ -217,7 +217,7 @@ class MainPage:
 
             # Basic setters
             toAdd.setIcon(resultList[0].getIcon())
-            toAdd.setName(resultList[0].getName())
+            toAdd.setName(term)
             toAdd.setCurrency(resultList[0].getCurrency())
             
             # Iterate through and calculate each value
