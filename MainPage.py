72000d5d5fcbb007227855e22f58262d08052228
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2026-02-24 17:20:32 +0800

    Forgot about encapsulation

--- MainPage.py
+++ MainPage.py
@@ -49,7 +49,7 @@ class MainPage:
             for j in range(0, len(toSort) - i - 1):
 
                 # Compare elements that are next to each other
-                if toSort[j].price > toSort[j + 1].price:
+                if toSort[j].getPrice() > toSort[j + 1].getPrice():
                     toSort[j], toSort[j + 1] = toSort[j + 1], toSort[j]
 
         return toSort
