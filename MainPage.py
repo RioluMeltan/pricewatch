98f24ac6c7d5d024e0a2cc2cdcf8b8b2fbf24824
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2026-02-25 20:26:55 +0800

    Streamlit's architecture sucks

--- MainPage.py
+++ MainPage.py
@@ -259,17 +259,17 @@ class MainPage:
     def importList() -> None: 
 
         # Text pasting input
-        toCheck = st.sidebar.text_input('Paste watchlist here: ')
+        toCheck = st.sidebar.text_area('Paste watchlist here: ')
         
-        # Iterate through formatted content using a text input
-        for i in toCheck.splitlines(): 
-
-            # Append to list
-            try: 
-                st.session_state.watches.append(Watch(i.split(',.,. ')[0], i.split(',.,. ')[1], i.split(',.,. ')[2], i.split(',.,. ')[3], i.split(',.,. ')[4], i.split(',.,. ')[5], i.split(',.,. ')[6], i.split(',.,. ')[7], i.split(',.,. ')[8], i.split(',.,. ')[9], i.split(',.,. ')[10], i.split(',.,. ')[11], i.split(',.,. ')[12], i.split(',.,. ')[13]))
-            except Exception as ex: 
-                st.toast('An error occurred. Ensure that your pasted list is formatted correctly.')
-                st.error(ex)
+        # Iterate through formatted content if an input is detected
+        if toCheck.strip(): 
+            for i in toCheck.splitlines(): 
+    
+                # Append to list
+                try: 
+                    st.session_state.watches.append(Watch(i.split(',.,. ')[0], i.split(',.,. ')[1], i.split(',.,. ')[2], i.split(',.,. ')[3], i.split(',.,. ')[4], i.split(',.,. ')[5], i.split(',.,. ')[6], i.split(',.,. ')[7], i.split(',.,. ')[8], i.split(',.,. ')[9], i.split(',.,. ')[10], i.split(',.,. ')[11], i.split(',.,. ')[12], i.split(',.,. ')[13]))
+                except Exception as ex: 
+                    st.toast('An error occurred. Ensure that your pasted list is formatted correctly.')
 
     # Static method to export a list to clipboard
     @staticmethod
